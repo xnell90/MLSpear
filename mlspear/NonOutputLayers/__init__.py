a85f6814236439f85685aa0c31058743173bce30
Alwyn Lopez <alwyn333@hotmail.com>
2020-04-05 11:01:56 -0400

    Added LeakyReLU layer

--- mlspear/NonOutputLayers/__init__.py
+++ mlspear/NonOutputLayers/__init__.py
@@ -1,4 +1,5 @@
 from mlspear.NonOutputLayers.BatchNormalization import *
+from mlspear.NonOutputLayers.LeakyReLU import * 
 from mlspear.NonOutputLayers.PReLU import *
 from mlspear.NonOutputLayers.ReLU import *
 from mlspear.NonOutputLayers.Tanh import *
